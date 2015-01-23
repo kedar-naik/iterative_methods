1e874e3a3d9f2fa24e214b0558f55e6dc9d48588
kedar-naik <knaik@stanford.edu>
2015-01-22 23:45:09 -0800

    working BCG_method
    BiConjugate Gradient method has been coded

--- solving_systems.py
+++ solving_systems.py
@@ -6,12 +6,18 @@ Created on Wed Jan 14 16:48:37 2015
 """
 
 # Solving linear systems
+from matplotlib import pylab
+
+pylab.close("all")
 
 # The system
-A = [[2, -1, 0], [-1, 2, -1], [0, -1, 2]]
+A = [[2, -1, 0], [-0.8, 2, -1], [0, -1, 2]]
 b = [32, 71, -42]
 
 # Using the Conjugate Gradient method
 from iterative_methods import CG_method
 x = CG_method(A, b)
 
+# Using the BiConjugate method
+from iterative_methods import BCG_method
+x = BCG_method(A,b)
