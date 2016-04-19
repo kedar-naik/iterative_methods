802e99640e4c11aee102d0523e1f0e9fd0ef110b
kedar-naik <knaik@stanford.edu>
2016-04-19 03:15:06 -0700

    HB spectral interpolation working

--- HB_practice.py
+++ HB_practice.py
@@ -11,16 +11,16 @@ from time_spectral import myLinspace
 from matplotlib import animation         # for specifying the writer
 import numpy as np
 
-# frequencies in the underlying signal
-actual_freqs = [1.0, 2.3]
+# angular frequencies in the underlying signal
+actual_omegas = [1.0, 3.5]
 
-# frequencies input by the user to the HB method
-freqs = [1.0, 2.3]
+# angular frequencies input by the user to the HB method
+omegas = [1.0, 3.5]
 
 
 
 #-----------------------------------------------------------------------------#
-def period_given_freqs(freqs):
+def period_given_freqs(omegas):
     '''
     if the frequencies that make up your solution are all rational numbers, the
     resulting signal is a periodic function. given that set of frequencies, 
@@ -29,20 +29,20 @@ def period_given_freqs(freqs):
     import decimal
     from functools import reduce
     # find the maximum number of decimal points seen the given frequencies
-    decimal_pts = [-decimal.Decimal(str(f)).as_tuple().exponent for f in freqs]
+    decimal_pts = [-decimal.Decimal(str(f)).as_tuple().exponent for f in omegas]
     max_dec_pts = max(decimal_pts)
     # multiply all frequencies by the power of ten that make them integers
-    scaled_freqs = [int(freq*pow(10,max_dec_pts)) for freq in freqs]
+    scaled_omegas = [int(omega*pow(10,max_dec_pts)) for omega in omegas]
     # find the greatest common divisor of these scaled frequencies
-    GCD = reduce(lambda x,y: math.gcd(x,y), scaled_freqs)
+    GCD = reduce(lambda x,y: math.gcd(x,y), scaled_omegas)
     # compute the multiple of the lowest-frequency period
-    lowest_multiple = min(scaled_freqs)/GCD
-    lowest_freq_period = 2.0*math.pi/min(freqs)
+    lowest_multiple = min(scaled_omegas)/GCD
+    lowest_omega_period = 2.0*math.pi/min(omegas)
     # find the total period
-    total_period = lowest_freq_period*lowest_multiple
+    total_period = lowest_omega_period*lowest_multiple
     return total_period
 #-----------------------------------------------------------------------------#
-def my_non_periodic_fun(t, freqs):
+def my_non_periodic_fun(t, omegas):
     '''
     given a set of specfied frequencies and time points, this subroutine 
     samples a function composed of a sum of sinusoids oscillating at the 
@@ -52,17 +52,17 @@ def my_non_periodic_fun(t, freqs):
     df = []
     # run through all the time samples
     for t_i in t:
-        f_i = 0
+        f_i = 0 + 5
         df_i = 0
         # run through all the frequencies
-        for freq in freqs:
-            f_i += math.sin(freq*t_i)
-            df_i += freq*math.cos(freq*t_i)
+        for omega in omegas:
+            f_i += math.sin(omega*t_i)
+            df_i += omega*math.cos(omega*t_i)
         f.append(f_i)
         df.append(df_i)
     return f, df
 #-----------------------------------------------------------------------------#
-def my_non_periodic_ode(t, u, freqs):
+def my_non_periodic_ode(t, u, omegas):
     
     '''
     given a set of specfied frequencies and time points, this subroutine 
@@ -76,11 +76,11 @@ def my_non_periodic_ode(t, u, freqs):
     # expression giving the derivative    
     dudt = u*(1-(u/C))
     # run through all the frequencies
-    for freq in freqs:
-        dudt = dudt - math.sin(freq*t)
+    for omega in omegas:
+        dudt = dudt - math.sin(omega*t)
     return dudt
 #-----------------------------------------------------------------------------#
-def harmonic_balance_operator(freqs):
+def harmonic_balance_operator(omegas):
     '''
     given a discrete set of frequencies, compute the harmonic-balance 
     differential operator matrix
@@ -88,16 +88,16 @@ def harmonic_balance_operator(freqs):
     import numpy as np
     from functools import reduce
     # set the number of the time instances
-    K = len(freqs)
+    K = len(omegas)
     N = 2*K+1
     # time period corresponding to the lowest frequency
-    T_lowest_freq = (2.0*math.pi)/min(freqs)
+    T_lowest_omega = (2.0*math.pi)/min(omegas)
     # set the location of the time instances 
-    delta_t = T_lowest_freq/N
+    delta_t = T_lowest_omega/N
     t_HB = np.array([i*delta_t for i in range(N)])
     # create a list of all N discrete frequencies
     # f = [0, f_1, ..., f_K, -f_K, ..., -f_1]
-    f = [0]+freqs+[-freqs[-(i+1)] for i in range(K)]
+    f = [0]+omegas+[-omegas[-(i+1)] for i in range(K)]
     # create the diagonal matrix holding the frequencies
     f_imag = [1j*f_i for f_i in f]
     D = np.diag(f_imag)
@@ -114,92 +114,84 @@ def harmonic_balance_operator(freqs):
     # return the operator matrix and the time instances
     return (D_HB, t_HB)
 #-----------------------------------------------------------------------------#
-# this function interpolates a series of points with a Fourier series #########
-def fourierInterp_given_freqs(x, y, freqs, x_int=None):
-    
-    """
+def fourierInterp_given_freqs(x, y, omegas, x_int=None):
+    '''
     This function interpolates a given set of ordinates and abscissas with a
-    Fourier series. The function returns a Fourier interpolation (of the 
-    highest degree trig polynomial allowed by the Nyquist Criterion) on the 
-    given grid of new abscissas. If no vector of desired abscissas is given, 
-    the set of interpolant abscissas is set automatically to be ten times as 
-    finely spaced as the original. The first derivative of the interpolant is 
-    also returned. Note that the interpolants will only be exact if the given 
-    points are just one shy of representing an exact period.
+    Fourier series that uses a specific set of frequencies. The interpolation 
+    is constructed using coefficients found for the cosine and sine terms by
+    solving a linear system built up from the given abscissas and ordinates.
     
     Input:
       - abscissas, x (as a list) (leave out last, duplicate point in period)
       - ordinates, y (as a list) (again, leave out last point, if periodic)
+      - angular frequencies, omegas (as a list)
       - new abscissas, x_int (as a list) (optional! defaults to 10x refinement)
     Output:
       - new abscissas, x_int (as a list)
       - interpolated ordinates, y_int (as a list)
       - derivative of the interpolant, dydx_int (as a list)
-    """
-    
+    '''
     import math
-    
+    import numpy as np
     # refinment factor for the interpolant. (If there are originally 10 points
     # but you want the Fourier Series to be defined on 50 points, then the 
     # refinement factor is 5.)
     refine_fac = 10
-    
-    # preliminaries
-    n = len(x)                  # number of abscissas
-    n = float(n)                # for mathematical clarity in the next line
-    m = math.floor((n-1)/2)     # highest degree trig polynomial allowed by NC
-    
-    # establish scaling to the domain [0,2pi)
-    x_interval = x[1]-x[0]      # even interval in abscissas
-    period = n*x_interval       # extrapolate and find length of the period
-    
-    # initalization
-    n = int(n)          # recast as int
-    m = int(m)          # recast as int
-    a = [0.0]*(m+1)       # "+1" in order to incorporate "DC value" at a_0
-    b = [0.0]*(m+1)       # b_0 never used, but included to match length of a
-    
-    # compute the coefficients
-    index = 0
-    for j in freqs:
-        a[index] = 0.0
-        b[index] = 0.0
-        for i in range(n):
-            scaled_x = (2*math.pi/period)*(x[i]-x[0])
-            a[index] += (2.0/n)*math.cos(j*scaled_x)*y[i]
-            b[index] += (2.0/n)*math.sin(j*scaled_x)*y[i]
-        index += 1
-        
+    # number of points passed in
+    n = len(x)                  
+    # if zero has not been included as a frequency, add it
+    if 0 not in omegas:
+        omegas = [0.0] + omegas
+    # total number of frequencies being considered, including the D.C. value
+    m = len(omegas)
+    # compute the coefficients by setting up and solving a linear system
+    N = len(x)
+    A = np.zeros((N,N))
+    b = np.zeros((N,1))
+    for i in range(N):
+        b[i] = y[i]
+        for j in range(N):
+            if j == 0:
+                A[i][j] = 1
+            else:
+                if j%2 == 1:
+                    A[i][j] = math.cos(omegas[int((j+1)/2)]*x[i])
+                else:
+                    A[i][j] = math.sin(omegas[int(j/2)]*x[i])
+    Fourier_coeffs = np.linalg.solve(A,b)
+    # create separate lists for the cosine and sine coefficients
+    a = []
+    b = [0]
+    for i in range(N):
+        if i==0 or i%2==1:
+            a.append(Fourier_coeffs[i][0])
+        else:
+            b.append(Fourier_coeffs[i][0])
     # set x_int, if it hasn't been given
     if x_int == None:
         n_int = refine_fac*(n)
-        x_int = myLinspace(x[0],x[-1]+x_interval,n_int)
+        x_int = myLinspace(x[0],x[-1]+x[1],n_int)
     else:
         n_int = len(x_int)
-    
     # find the actual interpolation
     y_int = [0.0]*n_int
     dydx_int = [0.0]*n_int
     for i in range(n_int):
         y_int[i] = a[0]/2.0    # the "DC" value
         dydx_int[i] = 0.0      # intialize the summation
-        scaled_x_int = (2*math.pi/period)*(x_int[i]-x[0])
-        index = 0
-        for j in freqs:
-            y_int[i] += a[index+1]*math.cos((j+1)*scaled_x_int) + \
-                        b[index+1]*math.sin((j+1)*scaled_x_int)
-            dydx_int[i] += (2*math.pi/period)*(j+1)* \
-                           (b[index+1]*math.cos((j+1)*scaled_x_int) - \
-                            a[index+1]*math.sin((j+1)*scaled_x_int))
-            index += 1
+        for j in range(m-1):
+            y_int[i] += a[j+1]*math.cos(omegas[j+1]*x_int[i]) + \
+                        b[j+1]*math.sin(omegas[j+1]*x_int[i])
+            dydx_int[i] += omegas[j+1]* \
+                           (b[j+1]*math.cos(omegas[j+1]*x_int[i]) - \
+                            a[j+1]*math.sin(omegas[j+1]*x_int[i]))
     return (x_int, y_int, dydx_int)
-    
-    
+#-----------------------------------------------------------------------------#    
     
     
 # compute and print the periods of the various signals
-T_HB_sol = period_given_freqs(freqs)
-T_actual_sol = period_given_freqs(actual_freqs)
+T_HB_sol = period_given_freqs(omegas)
+T_actual_sol = period_given_freqs(actual_omegas)
 print('\nPeriod of HB solution:', round(T_HB_sol,3))
 print('\nPeriod of the ODE solution:', round(T_actual_sol,3),'\n')
 
@@ -208,26 +200,27 @@ print('\nPeriod of the ODE solution:', round(T_actual_sol,3),'\n')
 ###############################################################################
 
 # create the harmonic balance operator matrix
-D_HB, t_HB = harmonic_balance_operator(freqs)
-print('D_HB = ',D_HB)
+D_HB, t_HB = harmonic_balance_operator(omegas)
+print('D_HB = ',D_HB,'\n')
 
 ###############################################################################
 # [harmonic balance] Check to see if Harmonic Balance operator is working #####
 ###############################################################################
 
 # [exact] time period corresponding to the lowest frequency being used for HB
-T_lowest_freq = (2.0*math.pi)/min(freqs)
+T_lowest_omega = (2.0*math.pi)/min(omegas)
 # [exact] discretize that longest time period
 points_per_unit_time = 100
-t = myLinspace(0, T_lowest_freq,points_per_unit_time*math.ceil(T_lowest_freq))
-f,df = my_non_periodic_fun(t, actual_freqs)
+t = myLinspace(0, T_lowest_omega,points_per_unit_time*math.ceil(T_lowest_omega))
+f,df = my_non_periodic_fun(t, actual_omegas)
 
 
 # [HB] checking to see if we can find time derivatives
-f_HB, dummy = my_non_periodic_fun(t_HB, actual_freqs)
+f_HB, dummy = my_non_periodic_fun(t_HB, actual_omegas)
 # turn it into a numpy column vector
 f_HB = np.array(f_HB)
 f_HB = f_HB.reshape(f_HB.size,1)
+# multiply by the HB operator matrix 
 df_HB = np.dot(D_HB, f_HB)
 
 # plot everything
@@ -235,17 +228,14 @@ plt.close('all')
 plt.figure()
 plt.plot(t, f, label='$f_{exact}$')
 plt.plot(t_HB, f_HB, 'ko', label='$f_{HB}$')
-plt.plot(t, df, 'r--', label='$df/dt_{exact}$')
+plt.plot(t, df, 'r-', label='$df/dt_{exact}$')
 plt.plot(t_HB, df_HB, 'go', label='$df/dt_{HB}$')
-
-print('t = ', np.array(t_HB))
-print('f = ', f_HB)
-
-t_HB_int,df_HB_int,dummy = fourierInterp_given_freqs(t_HB,df_HB,freqs)
-plt.plot(t_HB_int,df_HB_int, 'g--')
+t_HB_int, df_HB_int, dummy = fourierInterp_given_freqs(t_HB,df_HB,omegas)
+plt.plot(t_HB_int,df_HB_int, 'g--', label='$spectral\,\,interp.$')
 plt.xlabel('$t$', fontsize=16)
 plt.ylabel('$f(t)$', fontsize=16)
 plt.legend(loc='best')
+plt.title('$\omega_{actual} = \{'+str(actual_omegas)[1:-1]+'\} \quad\quad \omega_{used} = \{'+str(omegas)[1:-1]+'\}$', fontsize=16)
 
 ###########################################################################
 # [time accurate] explicit euler ##########################################
@@ -268,7 +258,7 @@ for n in range(time_points):
         f.append(initial_value)
     else:
         # explicitly step forward in time (give it the actual frequencies!)
-        f.append(f[n-1] + delta_t*my_non_periodic_ode(times[n-1],f[n-1],actual_freqs))
+        f.append(f[n-1] + delta_t*my_non_periodic_ode(times[n-1],f[n-1],actual_omegas))
 
 # plotting: USER INPUTS! do you want to animate the solution history or just
 # plot the final result? (True = animate, False = just print final result)
@@ -287,8 +277,8 @@ vertical_padding = (max(f)-min(f))/4.0
 plt.ylim(min(f)-vertical_padding,max(f)+vertical_padding)
 title = ''
 counter=1
-for freq in freqs:
-    title = title + '$\omega_{'+str(counter)+'} ='+str(freq)+'\quad $'
+for omega in omegas:
+    title = title + '$\omega_{'+str(counter)+'} ='+str(omega)+'\quad $'
     counter += 1
 plt.title(title+'$\Delta t = \,$'+str(delta_t))
 # plotting: set the total number of frames
